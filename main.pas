8694a10b7bc1ff8e49864e2d075d955fb8911cdd
NickTikhomirov <nicktikhomirov02@gmail.com>
2020-02-12 13:00:36 +0300

    added app class

--- main.pas
+++ main.pas
@@ -1,6 +1,40 @@
 uses graphabc;
 uses MyButtons;
 uses Factory;
+uses Screen;
+
+
+type
+  App = class
+  public
+    ScreenCurrent : IScreen;
+    exitFlag: boolean;
+    
+    constructor Create;
+    begin
+      self.ScreenCurrent := new ScreenMainMenu;
+    end;
+    
+    procedure execute;
+    begin
+      exitFlag := false;
+      repeat
+        window.Clear(clRed);
+        redraw();
+      until(exitFlag);
+    end;
+  end;
+
+
+//sry im too lazy to write singletones with pascalabc
+var AppObject : App;
+
+
 
 begin
+  lockdrawing();
+  window.SetSize(800, 500);
+  font.Name := 'Consolas';
+  AppObject := new App;
+  AppObject.execute();
 end.
