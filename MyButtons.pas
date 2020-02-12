f8d9655f7db2c8bec56c616eb43ed7f7a7f30f5d
NickTikhomirov <nicktikhomirov02@gmail.com>
2020-02-12 11:00:21 +0300

    added main and IButton

--- MyButtons.pas
+++ MyButtons.pas
@@ -2,6 +2,15 @@ unit MyButtons;
 
 interface
 
+uses graphabc;
+
+type
+  IButton = interface
+  public
+    function isHovered(xy:point):boolean;
+  end;
+
+
 implementation
 
 end.
---
+++ main.pas
@@ -0,0 +1,5 @@
+uses graphabc;
+uses MyButtons;
+
+begin
+end.
